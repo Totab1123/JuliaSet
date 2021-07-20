0429a2295ecca48f9f482e38241e95d224d58c0b
strvworks <strv13570@gmail.com>
2021-07-20 09:07:11 +0900

    Separated the Julia set drawing process into function

--- juliaset_sfml.cpp
+++ juliaset_sfml.cpp
@@ -29,11 +29,10 @@ int zeroOneToIndex(const float inValue, const uint step)
     return inValue / normalizedStep;
 }
 
-const sf::Color colorTable[] = {
-    DEFAULT_BACKGROUND_COLOR, { 0, 0, 255 }, { 0, 255, 0 },
-    { 0, 255, 255 },          { 255, 0, 0 }, { 255, 0, 255 },
-    { 255, 255, 0 }
-};
+const sf::Color colorTable[] = { { 0, 0, 0, 0 }, { 0, 0, 255 },
+                                 { 0, 255, 0 },  { 0, 255, 255 },
+                                 { 255, 0, 0 },  { 255, 0, 255 },
+                                 { 255, 255, 0 } };
 
 template<typename T, uint L>
 constexpr uint getArrayLength(const T (&)[L])
@@ -48,16 +47,47 @@ sf::Vector2<To> convertVector(const sf::Vector2<From> inValue)
                            static_cast<To>(inValue.y));
 }
 
+void drawJulia(sf::RenderWindow& window,
+               const sf::Vector2i drawPos,
+               const float stepSize = 0.005,
+               const uint maxItr = 300,
+               const float drawScale = 300)
+{
+    const double r = 0.27334;
+    const double i = 0.00742;
+    const int startAreaPos = -2;
+    const int endAreaPos = 2;
+    const uint totalStep = (endAreaPos - startAreaPos) / stepSize;
+    const auto tableLen = getArrayLength(colorTable);
+
+    sf::RectangleShape player(sf::Vector2f(1.0f, 1.0f));
+    for (int y = 0; y < totalStep; y++)
+    {
+        for (int x = 0; x < totalStep; x++)
+        {
+            const float n = startAreaPos + stepSize * y;
+            const float m = startAreaPos + stepSize * x;
+            if (const float v = julia(m, n, r, i, maxItr); v != 0)
+            {
+                const auto p =
+                  drawPos + (sf::Vector2i(m * drawScale, n * drawScale));
+                player.setPosition(p.x, p.y);
+                const auto fillColor = colorTable[zeroOneToIndex(v, tableLen)];
+                player.setFillColor(fillColor);
+                window.draw(player);
+            }
+        }
+        window.display();
+    }
+}
+
 int main()
 {
-    auto middle = DEFAULT_WINDOW_SIZE / 2u;
 
     sf::RenderWindow window(
       sf::VideoMode(DEFAULT_WINDOW_SIZE.x, DEFAULT_WINDOW_SIZE.y),
       "SFML juliaset",
       sf::Style::Default);
-    sf::RectangleShape player(sf::Vector2f(1.0f, 1.0f));
-    player.setFillColor(sf::Color(0, 0, 128, 128));
 
     bool completed = false;
     while (window.isOpen())
@@ -93,34 +123,7 @@ int main()
         if (!completed)
         {
             window.clear(DEFAULT_BACKGROUND_COLOR);
-            const double r = 0.27334;
-            const double i = 0.00742;
-            const float stepSize = 0.001;
-            const int startAreaPos = -2;
-            const int endAreaPos = 2;
-            const uint totalStep = (endAreaPos - startAreaPos) / stepSize;
-            const float drawScale = 400;
-            const uint maxItr = 500;
-
-            for (int y = 0; y < totalStep; y++)
-            {
-                for (int x = 0; x < totalStep; x++)
-                {
-                    const float n = startAreaPos + stepSize * y;
-                    const float m = startAreaPos + stepSize * x;
-                    if (const float v = julia(m, n, r, i, maxItr); v != 0)
-                    {
-                        player.setPosition(middle.x + m * drawScale,
-                                           middle.y + n * drawScale);
-                        const auto tableLen = getArrayLength(colorTable);
-                        const auto fillColor =
-                          colorTable[zeroOneToIndex(v, tableLen)];
-                        player.setFillColor(fillColor);
-                        window.draw(player);
-                    }
-                }
-                window.display();
-            }
+            drawJulia(window, convertVector<int>(DEFAULT_WINDOW_SIZE / 2u), 0.005, 300, 400);
             completed = true;
             std::cout << "drew julia" << std::endl;
         }
